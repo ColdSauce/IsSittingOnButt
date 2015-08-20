8ac5de60538845d54992f88a92e646665cb1ec23
Stefan Aleksic <saleksic@twilio.com>
2015-08-19 23:18:09 -0700

    Initial initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+isPressed = False
+def getPressedHtml():
+	return "Pressed"
+
+def getNotPressedHtml():
+	return "Not Pressed"
+
+@app.route("/released")
+def released():
+	global isPressed
+	isPressed = False
+
+@app.route("/pressed")
+def pressed():
+	global isPressed
+	isPressed = True
+	
+@app.route("/")
+def index():
+	global isPressed
+
+	if isPressed:
+		return getPressedHtml()	
+	else:
+		return getNotPressedHtml()
+
+
+if __name__ == "__main__":
+    app.run()
