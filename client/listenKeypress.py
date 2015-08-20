b28f3d8798c90980046804a80eb4060acb974ce1
Stefan <stefan@stefanaleksic.com>
2015-08-19 23:53:12 -0700

    Add config to client instead of server. Barebones config in client

--- client/listenKeypress.py
+++ client/listenKeypress.py
@@ -1,8 +1,15 @@
 import Tkinter as tk
+import urllib2
+def get_server_ip():
+    pass
+def get_server_port():
+    pass
 
-def onKeyPress(event):
-    print "hello world"
+def on_key_press(event):
+    server_ip = get_server_ip()
+    server_port = get_server_port()
+    urllib2.urlopen("{0}:{1}".format(server_ip, server_port))
 
 root = tk.Tk()
-root.bind('<KeyPress>', onKeyPress)
+root.bind('<KeyPress>', on_key_press)
 root.mainloop()
