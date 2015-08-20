ed6ae0c6592f0d93757aa54c7c0ff512c8efcc85
Stefan <stefan@stefanaleksic.com>
2015-08-20 01:42:07 -0700

    Add ability to listen to keypress and call server every 5 seconds with updates

--- client/listenKeypress.py
+++ client/listenKeypress.py
@@ -1,6 +1,9 @@
 import Tkinter as tk
 import yaml
 import requests
+import tty, termios, sys
+import thread
+import time
 
 
 yaml_contents = None
@@ -20,19 +23,47 @@ def get_server_port():
     yaml_contents = get_yaml_contents()
     return yaml_contents["SERVER_PORT"]
 
-def on_key_press(event):
+def on_key_press():
     server_ip = get_server_ip()
     server_port = get_server_port()
     url_to_request = "http://{0}:{1}/pressed".format(server_ip, server_port)
     requests.get(url_to_request)
 
-def on_key_released(event):
+def on_key_released():
     server_ip = get_server_ip()
     server_port = get_server_port()
-    url_to_request = "http://{0}:{1}/pressed".format(server_ip, server_port)
+    url_to_request = "http://{0}:{1}/released".format(server_ip, server_port)
     requests.get(url_to_request)
 
-root = tk.Tk()
-root.bind('<KeyPress>', on_key_press)
-root.bind('<KeyRelease>', on_key_released)
-root.mainloop()
+def getchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+is_pressed = False
+
+def get_char_thread():
+    global is_pressed
+    while 1:
+        ch = getchar()
+        is_pressed = True
+
+def check_is_pressed(delay):
+    global is_pressed
+    while 1:
+        time.sleep(delay)
+        if is_pressed:
+            on_key_press()
+            is_pressed = False
+        else:
+            on_key_released()
+
+thread.start_new_thread(get_char_thread, ())
+thread.start_new_thread(check_is_pressed, (5,))
+while 1:
+    pass
