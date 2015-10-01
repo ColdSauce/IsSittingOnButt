87e013f3e7433843e78faa7e144e80d25ceafa9f
tnarla <trushithanarla@gmail.com>
2015-09-30 23:50:33 -0400

    finished frontend

--- server/main.py
+++ server/main.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from twitter import *
 
 app = Flask(__name__)
 
+
+t = Twitter(
+    auth=OAuth('3329033923-exeAQZYaPtyqjnuWqnpyTbKlVysLzmtJ23FnQhY', '8EpD3yvv4HaSOs7iS5bsx42Y7EUzYQ1lLxG4NTr11bbtc', 'kPf0HlyvBfqDcBZOocCgz4AJo', 'uA1hc2kne09Tvv2eloSKZbyCiYgc2j99aHOorazfWot3iiuRvZ'))
+
+# Get your "home" timeline
+t.statuses.home_timeline()
+
+# Get a particular friend's timeline
+t.statuses.user_timeline(screen_name="billybob")
+
+# to pass in GET/POST parameters, such as `count`
+t.statuses.home_timeline(count=5)
 
 isPressed = False
 def getPressedHtml():
@@ -29,6 +42,8 @@ def index():
 		return getPressedHtml()	
         else:
 		return getNotPressedHtml()
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
