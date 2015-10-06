24cd8bb9a13ff07d35506e2aa45735f76a13c428
Niraj Pant <nirajpant7@gmail.com>
2015-10-05 22:44:26 -0500

    Twitter Integration

--- server/main.py
+++ server/main.py
@@ -7,16 +7,8 @@ app = Flask(__name__)
 t = Twitter(
     auth=OAuth('3329033923-exeAQZYaPtyqjnuWqnpyTbKlVysLzmtJ23FnQhY', '8EpD3yvv4HaSOs7iS5bsx42Y7EUzYQ1lLxG4NTr11bbtc', 'kPf0HlyvBfqDcBZOocCgz4AJo', 'uA1hc2kne09Tvv2eloSKZbyCiYgc2j99aHOorazfWot3iiuRvZ'))
 
-# Get your "home" timeline
-t.statuses.home_timeline()
-
-# Get a particular friend's timeline
-t.statuses.user_timeline(screen_name="billybob")
-
-# to pass in GET/POST parameters, such as `count`
-t.statuses.home_timeline(count=5)
-
 isPressed = False
+
 def getPressedHtml():
     return render_template("isPressed.html")
 
@@ -34,12 +26,20 @@ def pressed():
 	global isPressed
 	isPressed = True
         return getPressedHtml()
-	
+
+def last_tweet_yes():
+  last_tweet = t.statuses.user_timeline(screen_name="stestefanfan")[0]['text']
+  if last_tweet == "yes":
+    return True
+  else:
+    return False
+
 @app.route("/")
 def index():
 	global isPressed
-	if isPressed:
-		return getPressedHtml()	
+  # Insert twitter account here
+	if last_tweet_yes() == True:
+		return getPressedHtml()
         else:
 		return getNotPressedHtml()
 
